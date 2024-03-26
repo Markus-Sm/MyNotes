66de721571153c35c33049688eeabb4dd17cd8bf
Marek Samofal <marek.samofal@gmail.com>
2024-03-26 19:00:38 +0100

    adding inital notes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,89 @@
+import { useState } from 'react'
+
+const initialNotes = [
+	{
+		id: 1332,
+		topic: 'Inne',
+		text: 'Make a list of things needed to go to Germany for wark',
+	},
+	{
+		id: 1333,
+		topic: 'Zakupy',
+		text: 'Make a list of things to buy for the wedding',
+	},
+	{
+		id: 1334,
+		topic: 'Work',
+		text: 'Get three days of work in April at your job as a waiter',
+	},
+]
+
 function App() {
+	const [notes, setNotes] = useState(initialNotes)
+	const [showPanel, setShowPanel] = useState(false)
+
+	function hadnleShowPanel() {
+		setShowPanel(true)
+	}
+
+	function handleClosePanel() {
+		setShowPanel(false)
+	}
+
+	function handleClearNote() {
+		setNotes([])
+	}
+
 	return (
 		<div className='App'>
-			<Menu />
-			<NoteArea />
-			<NotePanel />
+			<Menu onShowPanel={hadnleShowPanel} onClearPanel={handleClearNote} />
+			<NoteArea notes={notes} />
+			{showPanel && <NotePanel onClosePanel={handleClosePanel} />}
 		</div>
 	)
 }
 
 export default App
 
-function Menu() {
+function Menu({ onShowPanel, onClearPanel }) {
 	return (
 		<div className='menu'>
 			<h1>🗒️ MyNotes</h1>
 			<div className='menu-buttons'>
-				<button className='add icon'>➕ Add +</button>
-				<button className='delete-all icon'>🗑️ Delete all</button>
+				<button onClick={onShowPanel} className='add icon'>
+					➕ Add
+				</button>
+				<button onClick={onClearPanel} className='delete-all icon'>
+					🗑️ Delete all
+				</button>
 			</div>
 		</div>
 	)
 }
 
-function NoteArea() {
+function NoteArea({ notes }) {
 	return (
 		<div className='note-area'>
-			<div className='note'>
-				<div className='note-header'>
-					<h3 className='note-title'>Notatka #1</h3>
-					<button className='delete-note'>❌</button>
-				</div>
-				<div className='note-body'>lorem</div>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit esse, neque possimus expedita deserunt delectus.
-			</div>
+			{notes.map(note => (
+				<Note note={note} key={note.id} />
+			))}
 		</div>
 	)
 }
 
-function NotePanel() {
+function Note({ note }) {
+	return (
+		<div className='note'>
+			<div className='note-header'>
+				<h3 className='note-title'>{note.topic}</h3>
+				<button className='delete-note'>❌</button>
+			</div>
+			<div className='note-body'>{note.text}</div>
+		</div>
+	)
+}
+
+function NotePanel({ onClosePanel }) {
 	return (
 		<div className='note-panel'>
 			<h2>Dodaj notatkę</h2>
@@ -58,7 +104,9 @@ function NotePanel() {
 
 			<div className='panel-buttons'>
 				<button className='save icon'>⬇️ Save</button>
-				<button className='cancel icon'>❌ Cancel</button>
+				<button onClick={onClosePanel} className='cancel icon'>
+					❌ Cancel
+				</button>
 			</div>
 		</div>
 	)
