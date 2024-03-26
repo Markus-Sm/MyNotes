6876c1a00fb617eede3f2deeefa87b3c625b5c5d
Marek Samofal <marek.samofal@gmail.com>
2024-03-26 17:04:33 +0100

    building layout: menu component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<Menu />
+		</div>
+	)
 }
 
 export default App
+
+function Menu() {
+	return (
+		<div className='menu'>
+			<h1>🗒️ MyNotes</h1>
+			<div className='menu-buttons'>
+				<button className='add icon'>➕ Add +</button>
+				<button className='delete-all icon'>🗑️ Delete all</button>
+			</div>
+		</div>
+	)
+}
